6d5f3521fb34f1d406b073f4ec2aee8e1f1db7a3
Kah919 <kahyap13@gmail.com>
2020-06-11 03:14:29 -0400

    Implement CSSTransition

--- src/components/Navbar/DropdownItem.js
+++ src/components/Navbar/DropdownItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default ({children}) => {
+export default ({children, leftIcon, rightIcon}) => {
     return(
         <a href="#" className="menu-item">
+            {/* <span className="icon-button">{leftIcon}</span> */}
             {children}
+            {/* <span className="icon-button">{rightIcon}</span> */}
         </a>
     )
 }
--- src/components/Navbar/DropdownMenu.js
+++ src/components/Navbar/DropdownMenu.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import DropdownItem from './DropdownItem';
+import { CSSTransition } from 'react-transition-group';
 
 export default () => {
+    const [activeMenu, setActiveMenu] = useState('main')
     return(
         <div className="dropdown">
             <DropdownItem>My Profile</DropdownItem>
+            <DropdownItem leftIcon={"x"} rightIcon={"x"}>
+                
+            </DropdownItem>
         </div>
     )
 }
