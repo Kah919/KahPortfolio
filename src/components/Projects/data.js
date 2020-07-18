5a782b1d466dadc047169eccbc63bd35af176eb8
Kah919 <kahyap13@gmail.com>
2020-07-17 22:01:09 -0400

    Populate dataset

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,3 +1,35 @@
+import hangry1 from '../../Images/hangry1.jpg'
+import hangry2 from '../../Images/hangry2.jpg'
+import hangry3 from '../../Images/hangry3.jpg'
+import hangry4 from '../../Images/hangry4.jpg'
+
+import ohsnap2 from '../../Images/ohsnap2.jpg'
+import ohsnap3 from '../../Images/ohsnap3.jpg'
+import ohsnap4 from '../../Images/ohsnap4.jpg'
+
+import stock2 from '../../Images/stock2.jpg'
+import stock3 from '../../Images/stock3.jpg'
+
+import corona1 from '../../Images/corona1.jpg'
+import corona2 from '../../Images/corona2.jpg'
+import corona3 from '../../Images/corona3.jpg'
+import corona4 from '../../Images/corona4.jpg'
+
+import shop1 from '../../Images/shop1.jpg'
+import shop2 from '../../Images/shop2.jpg'
+import shop3 from '../../Images/shop3.jpg'
+import shop4 from '../../Images/shop4.jpg'
+import shop5 from '../../Images/shop5.jpg'
+
+
+
+
+
+
+
+
+
+
 const projectData = [
     { name: 'Guess Who Infinite',
       description: 'A game of Guess Who where users can choose their own category and play with friends',
@@ -22,17 +54,17 @@ const projectData = [
     { name: 'Hangry',
       description: 'See what recipes you can make and what your friends',
       technology: [],
-      images: [],
+      images: [hangry1, hangry2, hangry3, hangry4],
     },
     { name: 'Stock Simulator',
       description: 'Create an account with $5000 and watch your investments change',
       technology: [],
-      images: [],
+      images: [stock2, stock3],
     },
     { name: 'Oh Snap',
       description: 'Social media for photographers to upload their work and gain inspiration',
       technology: [],
-      images: [],
+      images: [ohsnap2, ohsnap3, ohsnap4],
     },  
 ]
 
