c8b49bfc1de94ab9e1b5cd8c7d0b826ff7f6c979
Kah919 <kahyap13@gmail.com>
2020-07-29 13:03:25 -0400

    Created a github contribution component

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Landing from './LandingPage/Landing';
 import Project from './Projects/ProjectContainer';
 import Contact from './Contact/Contact';
+import Calendar from './GitHubCalendar/Calendar';
+
+
 
 export default () => {
     return(
         <div className='main_container'>
             <Landing />
+            <Calendar />
             <Project />
             <Contact />
         </div>
---
+++ src/components/GitHubCalendar/Calendar.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import GitHubCalendar from 'react-github-calendar';
+import styles from './Calendar.module.css';
+import Context from '../theme-context';
+import cx from 'classnames';
+
+export default () => {
+    const { globalState, globalDispatch } = useContext(Context);
+
+    const isDark = () => {
+        if(globalState.dark) {
+            return cx(styles.container, styles.dark)
+        }
+
+        return styles.container
+    }
+    
+    return(
+        <div className={ isDark() }>
+            <GitHubCalendar username="kah919" blockSize={20} />
+        </div>
+    )
+}
